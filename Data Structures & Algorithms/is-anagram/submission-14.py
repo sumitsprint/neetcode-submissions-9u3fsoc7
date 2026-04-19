3c9ec5cc09b869de21c28f9f907d6bc9cd2aa6f5
sumitsprint <ncr2099@gmail.com>
2026-04-19 09:27:01 +0530

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        map1, map2 = {}, {}
+
+        for i in range(len(s)):
+            map1.update({s[i]: map1.get(s[i], 0) + 1})
+            map2.update({t[i]: map2.get(t[i], 0) + 1})
+
+        for n in map1:
+            if map1[n] != map2.get(n, 0):
+                return False
+        return True        
+                    
+
+        
