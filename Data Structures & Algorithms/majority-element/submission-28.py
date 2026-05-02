0b10a913cbc441d03128790289cfdbb6fd31700d
sumitsprint <ncr2099@gmail.com>
2026-05-03 05:14:56 +0530

    Add: majority-element - submission-28

---
+++ Data Structures & Algorithms/majority-element/submission-28.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        current = 0
+        for n in nums:
+            if count == 0:
+                current = n
+            if n == current:
+                count += 1
+            else:
+                count -= 1
+        return current                
+        
