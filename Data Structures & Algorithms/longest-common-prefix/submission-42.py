93d5fbda9b92f7c5859e63e38b6501dc0fc41cce
sumitsprint <ncr2099@gmail.com>
2026-05-03 04:51:59 +0530

    Add: longest-common-prefix - submission-42

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-42.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        ans = ""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return ans
+                    
+            ans += s[i]
+        return ans 
+
+        # tc- O(n*m)
+        # sc-O(m)
+        # res can grow up to the length of the shortest string (let’s call that m).
+
+# You don’t store all n strings again; you just iterate
+#  over them. Iteration doesn’t
+#   add extra memory beyond what’s already given in input.
+######'''
+
