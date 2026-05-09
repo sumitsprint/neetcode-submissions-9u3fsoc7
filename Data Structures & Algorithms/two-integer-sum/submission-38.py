277893ba2fb99beb41a105025cd417554c9d3d07
sumitsprint <ncr2099@gmail.com>
2026-05-09 05:31:04 +0530

    Add: two-integer-sum - submission-38

---
+++ Data Structures & Algorithms/two-integer-sum/submission-38.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        flyMap = {
+    
+        }
+        for i, n in enumerate(nums):
+            ele = target - n
+            if ele in flyMap:
+                return [flyMap[ele], i]
+            flyMap[n] = i
+                
+
+        
