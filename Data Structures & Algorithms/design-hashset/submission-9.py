54546bda3d73910392412809dfc719aff9b9d8f0
sumitsprint <ncr2099@gmail.com>
2026-05-20 19:55:31 +0530

    Add: design-hashset - submission-9

---
+++ Data Structures & Algorithms/design-hashset/submission-9.py
@@ -0,0 +1,33 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.size = 5
+        self.buckets = [[] for _ in range(self.size)]
+
+    def add(self, key: int) -> None:
+        index = key % self.size
+        if key not in self.buckets[index]:
+            self.buckets[index].append(key)
+            return
+        
+
+    def remove(self, key: int) -> None:
+        index = key % self.size
+        if key in self.buckets[index]:
+            self.buckets[index].remove(key)
+        return    
+
+
+        
+
+    def contains(self, key: int) -> bool:
+        index = key % self.size
+        return key in self.buckets[index]
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
