2ec6e649c5f14da5b8e317c37df1fe9ed8535239
sumitsprint <ncr2099@gmail.com>
2026-05-28 05:27:44 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        flyMap = {}
+        for i, n in enumerate(numbers):
+            diff = target - n
+            if diff in flyMap:
+                return [flyMap[diff], i+1]
+            flyMap[n] = i+1  
+        return
+        
