3333918950f629cf2970cf241ee428b30e76a857
sumitsprint <ncr2099@gmail.com>
2026-05-29 14:57:57 +0530

    Add: duplicate-integer - submission-24

---
+++ Data Structures & Algorithms/duplicate-integer/submission-24.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums)):
+            if i > 0 and nums[i] ==  nums[i - 1]:
+                return True
+            
+        return False        
+# T.C- O(n log n)
+# S.C - O(1) or O(n)
+# python's sort may use O(n) extra space internally.
