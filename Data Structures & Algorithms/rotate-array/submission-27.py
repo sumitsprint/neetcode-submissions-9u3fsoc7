f1271ec80105e63ac02adcde97fa6af53199ea48
sumitsprint <ncr2099@gmail.com>
2026-05-31 04:09:55 +0530

    Add: rotate-array - submission-27

---
+++ Data Structures & Algorithms/rotate-array/submission-27.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+       
+        n= len(nums)
+        k = k% n
+        def rever(left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+            return nums
+
+        rever(0,n-1)
+        rever(0, k-1)
+        rever(k,n-1)        
