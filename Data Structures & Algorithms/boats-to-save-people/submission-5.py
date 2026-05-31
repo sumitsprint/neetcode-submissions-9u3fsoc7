184bf17ccce226a084c3dd8cf42c3af4d7405e01
sumitsprint <ncr2099@gmail.com>
2026-05-31 14:23:48 +0530

    Add: boats-to-save-people - submission-5

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-5.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        people.sort()
+
+        n = len(people)
+        left = 0
+        right = n - 1
+
+        paired_boats = 0
+
+        while left < right:
+            total_weight = people[left] + people[right]
+
+            if total_weight <= limit:
+                paired_boats += 1
+                left += 1
+                right -= 1
+
+            else:
+                right -= 1
+
+        paired_people = paired_boats * 2
+        unpaired_people = n - paired_people
+
+        total_boats = paired_boats + unpaired_people
+
+        return total_boats
