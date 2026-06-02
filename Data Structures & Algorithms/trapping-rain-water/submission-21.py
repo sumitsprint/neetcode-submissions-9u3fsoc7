617c964a416959b57c2a97cd3da1d19a396256ba
sumitsprint <ncr2099@gmail.com>
2026-06-02 14:37:49 +0530

    Add: trapping-rain-water - submission-21

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-21.py
@@ -0,0 +1,23 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        left = 0
+        right = n - 1
+
+        left_max = height[left]
+        right_max = height[right]
+
+        water = 0
+
+        while left < right:
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                water += left_max - height[left]
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                water += right_max - height[right]
+        return water            
+        # tc- O(n)
+        # sc-O(1)
