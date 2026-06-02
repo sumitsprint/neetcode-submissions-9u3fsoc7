fba2fad3081673f68417fd61095910b03a1adf64
sumitsprint <ncr2099@gmail.com>
2026-06-03 05:13:25 +0530

    Add: merge-strings-alternately - submission-6

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        l = 0
+        r = 0
+        res = []
+
+        
+        while l< len(word1) and r < len(word2):
+            res.append(word1[l])
+            res.append(word2[r])
+            l += 1
+            r+= 1
+
+        res.append(word1[l:])
+        res.append(word2[r:])
+
+        return "".join(res)    
+            
+        
