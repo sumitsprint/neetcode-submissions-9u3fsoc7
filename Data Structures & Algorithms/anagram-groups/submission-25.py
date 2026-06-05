b29b9f495e8bbab7b929d75184956b96c426b353
sumitsprint <ncr2099@gmail.com>
2026-06-05 13:46:44 +0530

    Add: anagram-groups - submission-25

---
+++ Data Structures & Algorithms/anagram-groups/submission-25.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map1 = defaultdict(list)
+        for s in strs:
+            fre = [0] * 26
+            for c in s:
+                fre[ord(c)-ord('a')] += 1
+            map1[tuple(fre)].append(s)    
+        return list(map1.values())    
+        
