6ce103255d19d8e9f3a0d04f57ca832d4e5ff71f
sumitsprint <ncr2099@gmail.com>
2026-06-10 05:21:21 +0530

    Add: longest-consecutive-sequence - submission-80

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-80.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        longest_streak = 0
+        seen = set(nums)
+
+        for n in seen:
+            if n - 1 in seen:
+                continue
+            current_streak = 1    
+            forward_element = n + 1
+            while forward_element in seen:
+                current_streak += 1
+                forward_element += 1
+            longest_streak = max(longest_streak, current_streak)
+        return longest_streak       
+
+
+        
