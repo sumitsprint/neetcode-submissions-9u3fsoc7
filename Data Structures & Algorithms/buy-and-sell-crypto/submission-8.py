747ffd5a4d7cca21d574f5f3195f6a115291da68
sumitsprint <ncr2099@gmail.com>
2026-06-14 13:42:27 +0530

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minprice = prices[0]
+        maxP = 0
+
+        for i in range(len(prices)):
+            minprice = min(minprice, prices[i])
+            profit = prices[i] - minprice
+            
+            maxP = max(maxP, profit)
+        return maxP  
+        if not prices:
+            return 0
+            
+        min_price = prices[0]
+        max_profit = 0
+
+        # Start from index 1 since index 0 is already our initial min_price
+        for i in range(1, len(prices)):
+            # Update the lowest purchase price seen so far
+            min_price = min(min_price, prices[i])
+            
+            # Calculate profit if we were to sell at the current price
+            current_profit = prices[i] - min_price
+            
+            # Update the maximum profit achieved so far
+            max_profit = max(max_profit, current_profit)
+            
+        return max_profit  
+
+
+                     
+        
