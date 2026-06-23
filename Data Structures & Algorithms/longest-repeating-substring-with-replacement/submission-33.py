a4ebbe8a10073a1004186e468feef94f87a8a31d
sumitsprint <ncr2099@gmail.com>
2026-06-24 05:04:47 +0530

    Add: longest-repeating-substring-with-replacement - submission-33

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-33.py
@@ -0,0 +1,50 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        fre = [0] * 26
+        left = 0
+        max_freq = 0
+        max_length = 0
+
+
+        # expand the window
+        for right in range(len(s)):
+            # feed freq
+            fre[ord(s[right]) - ord('A')] += 1
+            window_size = right - left + 1
+            max_freq = max(max_freq, fre[ord(s[right]) - ord('A')])
+
+            # check if window is valid
+
+            validity = window_size - max_freq
+
+            while validity > k and left < len(s):
+
+                fre[ord(s[left]) - ord('A')] -= 1
+                left += 1
+                window_size = right - left + 1
+                validity = window_size - max_freq
+            max_length = max(window_size, max_length)
+        return max_length        
+                
+
+
+
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+        
