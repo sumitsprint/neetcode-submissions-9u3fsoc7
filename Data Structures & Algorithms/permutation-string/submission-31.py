4bae8b01aeb58edadcf00054d4eff714b28fd168
sumitsprint <ncr2099@gmail.com>
2026-06-24 14:13:11 +0530

    Add: permutation-string - submission-31

---
+++ Data Structures & Algorithms/permutation-string/submission-31.py
@@ -0,0 +1,41 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            
+            return False
+
+        fre = [0] * 26
+
+        window_size = len(s1)
+        for i in range(len(s1)):
+            fre[ord(s1[i]) - ord('a')] += 1
+
+        for i in range(window_size):
+            fre[ord(s2[i]) - ord('a')] -= 1
+
+        if all(f == 0 for f in fre):
+            return True
+
+        left  = 0    
+
+        for right in range(window_size, len(s2)):
+            fre[ord(s2[left]) - ord('a')] += 1
+            left += 1
+
+            fre[ord(s2[right]) - ord('a')] -= 1
+            if all(f == 0 for f in fre):
+                return True
+
+        return False
+
+
+
+
+            
+
+
+
+
+
+
+        
