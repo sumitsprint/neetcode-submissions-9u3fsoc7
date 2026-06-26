480cde47a2a129c74691821e1c794e0e2a6e71cc
sumitsprint <ncr2099@gmail.com>
2026-06-26 13:54:06 +0530

    Add: valid-sudoku - submission-28

---
+++ Data Structures & Algorithms/valid-sudoku/submission-28.py
@@ -0,0 +1,46 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for row in board:
+            see= set()
+            for val in row:
+                if val == ".":
+                    continue
+                if val in see:
+                    return False
+                see.add(val) 
+
+        # for cols
+
+        for col in range(9):
+            see = set()
+            for row in range(9):
+                if board[row][col] == ".":
+                    continue
+                if board[row][col] in see:
+                    return False
+                see.add(board[row][col])
+
+        #for box
+
+        for i in range(0,9,3):
+            for j in range(0,9,3):
+                see = set()
+                for l in range(i,i+3):
+                    for k in range(j,j+3):
+                        if board[l][k] == ".":
+                            continue
+                        if board[l][k] in see:
+                            return False
+                        see.add(board[l][k])
+        return True                    
+
+
+
+
+
+
+
+
+                   
+
+        
