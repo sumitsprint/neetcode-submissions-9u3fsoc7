03a86c5e312cb4ea57661a3265e277faf13f73d7
sumitsprint <ncr2099@gmail.com>
2026-06-30 13:31:04 +0530

    Add: longest-substring-without-duplicates - submission-28

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-28.py
@@ -0,0 +1,27 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = set()
+        max_longest = 0
+        left = 0
+        
+        for i in range(len(s)):
+            
+            while s[i] in seen:
+                
+                
+                
+                seen.remove(s[left])
+                left += 1
+
+            seen.add(s[i])
+            #answer is recorded from valid state, never from a invalid state
+            max_longest = max(max_longest, i - left +1)
+        return max_longest    
+
+
+                
+
+# tc O(n)
+# sc O(min(n,m))
+        
+        
