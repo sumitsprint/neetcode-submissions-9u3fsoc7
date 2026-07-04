df0d948fd08333516fa7bdcea388b791ca022c99
sumitsprint <ncr2099@gmail.com>
2026-07-04 13:55:09 +0530

    Add: validate-parentheses - submission-46

---
+++ Data Structures & Algorithms/validate-parentheses/submission-46.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pairs = {
+
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+        stack = []
+
+        for c in s:
+            #closing bracket
+            if c in pairs:
+
+                top_ele = stack.pop() if stack else "#"
+
+                if top_ele != pairs[c]:
+                    return False
+
+            #opening be=racket
+
+            else:
+                stack.append(c)
+        return not stack                
+
+
+
+        
