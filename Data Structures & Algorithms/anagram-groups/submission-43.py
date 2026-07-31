b3b9a289a02fbc6b510ef1c631e5f9425a4c8346
sumitsprint <ncr2099@gmail.com>
2026-07-31 14:55:25 +0530

    Add: anagram-groups - submission-43

---
+++ Data Structures & Algorithms/anagram-groups/submission-43.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map1 = defaultdict(list)
+
+        for s in strs:
+            fre = [0] *26
+            for c in s:
+                fre[ord(c) - ord('a')] += 1
+            map1[tuple(fre)].append(s)
+        return list(map1.values())        
+
+
+# what if the key has never been seen before
+        #    tc - O(n*m)
+        # sc - O(n)
