9a9331e96b131f5fedc7b7e9f058a3cbc654519f
sumitsprint <ncr2099@gmail.com>
2026-08-03 14:58:41 +0530

    Add: design-hashmap - submission-64

---
+++ Data Structures & Algorithms/design-hashmap/submission-64.py
@@ -0,0 +1,51 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 1000
+        self.buckets = [[] for _ in range(self.size)]
+
+    def _hash(self, key):
+        if isinstance(key, str):
+            return sum(ord(c) for c in key) % self.size
+
+        else:
+            return key % self.size        
+        
+
+    def put(self, key: int, value: int) -> None:
+        index = self._hash(key)
+        for i, (k,v) in enumerate(self.buckets[index]):
+            if k == key:
+                self.buckets[index][i] = (key, value)
+                return
+        self.buckets[index].append((key, value))
+        return
+
+        
+
+    def get(self, key: int) -> int:
+        index = self._hash(key)
+        for i, (k, v) in enumerate(self.buckets[index]):
+            if k == key:
+                # self.buckets[index].append((key, value))
+                return v
+        return -1
+        # self.buckets[index].append((key, value))
+        # return
+        
+
+    def remove(self, key: int) -> None:
+        index = self._hash(key)
+        for i, (k,v) in enumerate(self.buckets[index]):
+            if k == key:
+                self.buckets[index].pop(i)
+                return
+        
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
