863724a496508c42d6312a11b6c27c246c854d38
sumitsprint <ncr2099@gmail.com>
2026-08-05 14:00:16 +0530

    Add: duplicate-integer - submission-40

---
+++ Data Structures & Algorithms/duplicate-integer/submission-40.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i] ==  nums[i - 1]:
+                return True
+            
+        return False        
+# T.C- O(n log n) bcoz of sorting
+# S.C - O(1) or O(n)
+# python's sort may use O(n) extra space internally.
