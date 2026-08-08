93400287f12361844654dbf7eb812705b5fe9798
sumitsprint <ncr2099@gmail.com>
2026-08-08 14:29:30 +0530

    Add: top-k-elements-in-list - submission-51

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-51.py
@@ -0,0 +1,34 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        occ = {}
+        for n in nums:
+            occ[n] = occ.get(n, 0) + 1
+
+        buckets = [[] for i in range(len(nums) + 1)]
+        for key, value in occ.items():
+            buckets[value].append(key)
+        
+        res = []
+        for i in range(len(buckets)-1,-1,-1):
+            for num in buckets[i]:
+                res.append(num)
+
+                if len(res) == k:
+                    return res
+#does not matter if both elements occur same number of time
+#index = frequency
+#distinct elements in sublists
+# len(nums)+1 because on 0 index no element exists            
+
+        #    TC - O(n)
+        #    SC - O(n)
+
+
+        
+
+
+
+
+
+        
