c840088855cc46f9391a1c6bbeee679ffac63084
sumitsprint <ncr2099@gmail.com>
2026-08-19 13:35:49 +0530

    Add: validate-parentheses - submission-59

---
+++ Data Structures & Algorithms/validate-parentheses/submission-59.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pairs = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+        st = []
+
+        for c in s:
+            if c in pairs:
+                if not st:
+                    return False
+                if  st[-1] != pairs[c]:
+                    return False
+                elif st[-1] == pairs[c]:
+                    st.pop()
+            else:
+                st.append(c)            
+            
+        return not st
+
