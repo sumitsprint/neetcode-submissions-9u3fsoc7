85c31007723a35ae44a20cf919dbad51a6051ef6
sumitsprint <ncr2099@gmail.com>
2026-08-29 20:06:25 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,44 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
+        while left <= right:
+            thr = 0
+            mid = (left + right) // 2 # candidate eating rate
+            for n in piles:
+                hr = n // mid
+                hro = n % mid
+                if hro > 0:
+
+                    hr += 1
+                
+                thr += hr 
+            if thr <= h:
+                right = mid - 1
+            else:
+                #If mid is too slow, which side of the search space contains the speeds that might work?
+                left = mid + 1
+
+                   
+
+                #right moves left whenever mid is valid.
+# left moves right whenever mid is too slow.
+        return left  
+
+
+
+
+            
+
+
+
+
+
+
+
+
+            
+
+
+        
+        
