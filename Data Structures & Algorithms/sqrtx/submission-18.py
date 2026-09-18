b46c8372d9997b79e3c09faea9bc60c4441a8d3f
sumitsprint <ncr2099@gmail.com>
2026-09-18 14:43:42 +0530

    Add: sqrtx - submission-18

---
+++ Data Structures & Algorithms/sqrtx/submission-18.py
@@ -0,0 +1,32 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left = 1
+        right =  x
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if (mid * mid) > x:
+                right = mid - 1
+
+            else:
+                left = mid + 1 #left moves whenever it is valid 
+
+        return right  # so on the left of left all are valid
+
+
+        
+         #largest
+
+         
+
+
+         
+         
+         
+                
+
+            
+
+
+        
