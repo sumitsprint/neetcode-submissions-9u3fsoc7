7e1ad200319648aa832c7d3eb5344092e2c3a61d
sumitsprint <ncr2099@gmail.com>
2026-09-26 14:57:13 +0530

    Add: two-integer-sum-ii - submission-28

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-28.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+        for i in range(len(numbers)):
+
+            complement = target - numbers[i]
+
+            left = i + 1
+            right = len(numbers) - 1
+
+            while left <= right:
+                mid = (left + right) // 2
+
+                if numbers[mid] == complement:
+                    return [i + 1, mid + 1]
+
+                elif numbers[mid] < complement:
+                    left = mid + 1
+
+                else:
+                    right = mid - 1
